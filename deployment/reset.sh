71a94e6d8608403c3a5017efd7a9f5346c26accb
manuelah123 <m.henao10@utp.edu.co>
2026-05-27 22:21:09 -0500

    Update reset.sh

--- deployment/reset.sh
+++ deployment/reset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "⚠️ Reseteando base de datos..."
+echo " Reseteando base de datos..."
 
 DB1=$(incus exec db-node1 -- hostname -I | awk '{print $1}')
 
@@ -11,4 +11,4 @@ incus exec db-node1 -- cockroach sql --insecure --host=$DB1:26257 -e "CREATE DAT
 # Cargar schema
 incus exec db-node1 -- cockroach sql --insecure --host=$DB1:26257 --database=parqueadero < ~/parking-project/database/init.sql
 
-echo "✅ Base de datos reseteada"
+echo " Base de datos reseteada"
